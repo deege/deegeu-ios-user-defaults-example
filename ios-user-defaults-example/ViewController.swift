ba59cefb0538de8a774298a812bb502ff8c2a3d2
DJ Spiess <dspiess@nso.edu>
2015-09-15 13:22:37 -0600

    Initial Commit

---
+++ ios-user-defaults-example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-user-defaults-example
+//
+//  Created by Daniel Spiess on 9/15/15.
+//  Copyright © 2015 Daniel Spiess. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
